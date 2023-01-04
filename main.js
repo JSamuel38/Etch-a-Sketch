3ede97f013d152384fa1a67f2cea53831e08d5e4
Samuel Evans <117552758+JSamuel38@users.noreply.github.com>
2023-01-04 20:12:43 +0000

    Add color picker and ability to draw

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+const colorPicker = document.createElement('input');
+colorPicker.setAttribute('type', 'color');
+colorPicker.textContent = 'Pick a color';
+document.body.appendChild(colorPicker);
+
+let pickedColor;
+colorPicker.addEventListener('change', (e) => {
+	pickedColor = e.target.value;
+});
+
 const container = document.querySelector(".container");
+let containerSize = 256;
 //Create every pixel in the container for a 16x16 canvas
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < containerSize; i++) {
 	container.appendChild(document.createElement("div")); 
 }
 
-const containerDiv = container.querySelectorAll("div")
-containerDiv.forEach((div) => {
+const pixel = container.querySelectorAll("div")
+pixel.forEach((div) => {
 	div.setAttribute("class", "pixel");
+});
+
+pixel.forEach((pixel) => {
+  pixel.addEventListener("mouseover", () => {
+		pixel.style.backgroundColor = pickedColor || "black";
+	});
 });
